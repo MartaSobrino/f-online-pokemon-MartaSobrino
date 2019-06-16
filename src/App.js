c90c54fbb2802d8beef59db8f0a4498d1177e7d0
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-06-16 13:45:01 +0200

    api data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ENDPOINT = 'http://pokeapi.salestock.net/api/v2/pokemon/?limit=25'
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dataList: [],
+      filterName: ''
+    }
+    this.fetchData = this.fetchData.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  componentDidMount(){
+    this.fetchData();
+  }
+
+  fetchData(){
+    fetch(ENDPOINT)
+    .then(response => response.json())
+    .then(data => {
+      data.results.map(item => 
+        fetch(item.url)
+        .then(response => response.json())
+        .then(datados=>{
+          this.setState({
+            dataList:  [...this.state.dataList, datados]
+          });
+          localStorage.setItem('data', JSON.stringify(this.state.dataList));
+        })
+    )
+  })
+  }
+
+  handleFilter(event){
+    const trigger = event.currentTarget.value;
+    this.setState ({
+      filterName: trigger
+    })
+  }
+
+  render(){
+    console.log(this.state.dataList);
+    const {dataList} = this.state;
+    return (
+      <React.Fragment>
+        <div className="field__container">
+          <label htmlFor=""></label>
+          <input type="text" name="" id=""/>
+        </div>
+        <div className="info__container">
+          <ul>
+            {dataList
+              .map(item=>
+                <li key={item.id}>
+                  <h2>{item.name}</h2>
+                  <p>{item.id}</p>
+                  <img src={item.sprites.front_default} alt="imagen_pokemon"/>
+                  <ul>{item.types
+                    .map(item => 
+                        <li key={item.type.name}>{item.type.name}</li>
+                    )}
+                  </ul>
+                </li>
+              )}
+          </ul>
+        </div>
+      </React.Fragment>
+      );
+  }
 }
 
 export default App;
